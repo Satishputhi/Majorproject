3f9b405e2328a54c60bf8a7c9ad54955afc4d4a5
260001 <260001@INNOITWFF3HXRN3.AMERICAS.GLOBAL.NTTDATA.COM>
2023-09-13 12:38:48 +0530

    commit message

---
+++ UserMangemnt-1/src/main/java/com/spring/UserMangemnt1Application.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMangemnt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMangemnt1Application.class, args);
+		System.out.println("Satish");
+	}
+
+}
---
+++ UserMangemnt-1/src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,87 @@
+package com.spring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+import com.spring.entity.UserAccount;
+import com.spring.service.UserServiceImp;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserServiceImp uI;
+	
+	@GetMapping("/")
+	 public String index(Model model) {
+		 model.addAttribute("user", new UserAccount());
+		return "index";
+		 
+	 }
+@PostMapping("/save-user")
+public String handleSubmitBtn(@ModelAttribute("user") UserAccount userAcc, Model model) {
+	
+	
+	String msg= uI.saveOrUpdateUserAcc(userAcc);
+	model.addAttribute("msg", msg);
+	return "index";
+	
+}
+@GetMapping("/user")
+public String viewUsers(Model model) {
+	List<UserAccount> list = uI.getAllUserAccounts();
+	model.addAttribute("users", list);
+	return "view-user";
+	}
+//@GetMapping("/edit/{userid}")
+//public String editUser(@PathVariable Integer userId, Model model) {
+//	   uI.getUserAcc(userId);
+//	   return "index";
+//	 
+// }
+@GetMapping("/edit/{userid}")
+public String editUserForm(@PathVariable("userid") Integer userId, Model model) {
+  UserAccount userAcc = uI.getUserAcc(userId);
+ model.addAttribute("user", userAcc);
+    return "edit";
+}
+
+@PostMapping("/edit/{userid}")
+public String editUser(@ModelAttribute("user") UserAccount userAcc, Model model) {
+    uI.saveOrUpdateUserAcc(userAcc);
+    return "redirect:/user/";
+}
+
+@GetMapping("/delete/{userid}")
+public String deleteUser(@PathVariable("userid") Integer userId) {
+    uI.deleteUserAcc(userId);
+    return "redirect:/user/";
+}
+@PostMapping("/toggleActivation")
+public String toggleActivation(Integer userId) {
+    uI.toggleUserActivation(userId);
+    return "redirect:/user/";
+}
+//
+// public String deleteUser(Integer userId, Model model) {
+//	return "list";
+//	 
+// }
+//
+//public String changeAccStatus(Integer userId, String status) {
+//	return status;
+//
+//}
+}
